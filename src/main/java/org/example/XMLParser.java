704cdd627b9e2dd7ae04925892d38b9c9dfadd9d
d.krivoruchko <krivoruchkodmitry@gmail.com>
2023-04-14 15:24:59 +0200

    Alle Daten werden über Terminal ausgegeben.

--- src/main/java/org/example/XMLParser.java
+++ src/main/java/org/example/XMLParser.java
@@ -15,24 +15,38 @@ public class XMLParser {
 
     public static void main(String[] args) {
         SAXBuilder builder = new SAXBuilder();
-        File xmlFile = new File("values.xml");
-
+        File xmlFile = new File("C:\\Users\\d.krivoruchko\\Documents\\Epikur\\Übungsaufgaben\\values.xml");
         try {
 
             Document document = builder.build(xmlFile);
             Element rootNode = document.getRootElement();
 
-
-            List<String> scaleNames = new ArrayList<>();
-            List<Double> values = new ArrayList<>();
+            List<Object> coordinates = new ArrayList<>();
             for (Element result : rootNode.getChildren("Result")) {
+                System.out.println(" ");
                 for (Element value : result.getChildren("Value")) {
                     String scale = value.getAttributeValue("Scale");
-                    double v = Double.parseDouble(value.getAttributeValue("V"));
-                    scaleNames.add(scale);
-                    values.add(v);
+                    Double v = Double.parseDouble(value.getAttributeValue("V"));
+                    coordinates.add(scale);
+                    coordinates.add(v);
+                    System.out.println("Scale: "+scale+" V: "+v);
                 }
             }
+
+            for (Element report : rootNode.getChildren("Report")) {
+                System.out.println(" ");
+                for (Element value : report.getChildren("Value")) {
+                    String scale = value.getAttributeValue("Scale");
+                    Double av = Double.parseDouble(value.getAttributeValue("AV"));
+                    Double dev = Double.parseDouble(value.getAttributeValue("DEV"));
+                    coordinates.add(scale);
+                    coordinates.add(av);
+                    coordinates.add(dev);
+                    System.out.println("Scale: "+scale+" AV: "+av+" DEV: "+dev);
+                }
+            }
+
+
 
         } catch (IOException | JDOMException e) {
             e.printStackTrace();
