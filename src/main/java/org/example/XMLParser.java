22df8ba81b9bb73cf02a89ec802ad04cc0adfd38
d.krivoruchko <krivoruchkodmitry@gmail.com>
2023-04-21 10:34:02 +0200

    Richtige grafische Orientierung.  Wird aber nur die letzt eingetragene XYSerie abgebildet

--- src/main/java/org/example/XMLParser.java
+++ src/main/java/org/example/XMLParser.java
@@ -12,9 +12,12 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
-
+/**
+ *
+ */
 public class XMLParser {
 
     public static void main(String[] args) {
@@ -25,21 +28,29 @@ public class XMLParser {
             Document document = builder.build(xmlFile);
             Element rootNode = document.getRootElement();
             XYSeriesCollection dataset = new XYSeriesCollection();
+            XYSeries xySeries = new XYSeries("Result");
+            XYSeries xySeries1 = new XYSeries("Report");
+            XYSeries xySeries2 = new XYSeries("Report Abweichung");
 
             List<Object> coordinates = new ArrayList<>();
             for (Element result : rootNode.getChildren("Result")) {
                 System.out.println(" ");
+                double i = 10;
                 for (Element value : result.getChildren("Value")) {
                     String scale = value.getAttributeValue("Scale");
                     Double v = Double.parseDouble(value.getAttributeValue("V"));
                     coordinates.add(scale);
                     coordinates.add(v);
                     System.out.println("Scale: "+scale+" V: "+v);
+
+                    xySeries.add(i, v);
+                    i--;
                 }
             }
 
             for (Element report : rootNode.getChildren("Report")) {
                 System.out.println(" ");
+                double i = 10;
                 for (Element value : report.getChildren("Value")) {
                     String scale = value.getAttributeValue("Scale");
                     Double av = Double.parseDouble(value.getAttributeValue("AV"));
@@ -48,15 +59,23 @@ public class XMLParser {
                     coordinates.add(av);
                     coordinates.add(dev);
                     System.out.println("Scale: "+scale+" AV: "+av+" DEV: "+dev);
+
+                    xySeries1.add(i, av);
+                    xySeries2.add(i, av-dev);
+                    i--;
                 }
             }
+
+            dataset.addSeries(xySeries);
+            dataset.addSeries(xySeries1);
+            dataset.addSeries(xySeries2);
 
             JFreeChart chart = ChartFactory.createXYLineChart(
                     "Patient Values",
                     "Value",
                     "Scale",
                     dataset,
-                    PlotOrientation.VERTICAL,
+                    PlotOrientation.HORIZONTAL,
                     false,
                     true,
                     false
