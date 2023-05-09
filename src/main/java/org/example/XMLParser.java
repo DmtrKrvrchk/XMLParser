7cb672145250f77aa46e23c12ad3a57fc0774249
d.krivoruchko <krivoruchkodmitry@gmail.com>
2023-05-09 12:11:44 +0200

    TODO #1: XY-Achse-Orientierung: Scale-Namen liegen an der falschen Axe

--- src/main/java/org/example/XMLParser.java
+++ src/main/java/org/example/XMLParser.java
@@ -3,9 +3,6 @@ package org.example;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
@@ -20,6 +17,7 @@ import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.DeviationRenderer;
 import org.jfree.data.xy.YIntervalSeries;
 import org.jfree.data.xy.YIntervalSeriesCollection;
+import javax.swing.*;
 
 
 /**
@@ -30,72 +28,50 @@ public class XMLParser {
     public static void main(String[] args) {
         SAXBuilder builder = new SAXBuilder();
         File xmlFile = new File("C:\\Users\\d.krivoruchko\\IdeaProjects\\XMLParser\\values.xml");
-        try {
 
+        try {
             Document document = builder.build(xmlFile);
             Element rootNode = document.getRootElement();
+            String[] scalesName = new String[10];
+
             YIntervalSeriesCollection dataset = new YIntervalSeriesCollection();
-            YIntervalSeries xySeries = new YIntervalSeries("Result");
-            YIntervalSeries xySeries1 = new YIntervalSeries("Report1");
-            YIntervalSeries xySeries2 = new YIntervalSeries("Report2");
-            YIntervalSeries xySeries3 = new YIntervalSeries("Report3");
+            YIntervalSeries resultYSeries = new YIntervalSeries("Result");
+            YIntervalSeries stationaerYSeries = new YIntervalSeries("Referenzwerte stationärer Psychotherapiepatienten");
+            YIntervalSeries hausGesamtYSeries = new YIntervalSeries("Referenzwerte von Hausarztpatienten (Gesamtstichprobe)");
+            YIntervalSeries hausTeilYSeries = new YIntervalSeries("Referenzwerte von Hausarztpatienten (Teilstichprobe Gesunde)");
 
-
-            String[] scaleValue = {"SOZB", "SOZU", "A_P", "SELB", "INT", "WOHL", "PSB", "PHO", "DEP", "SOM"};
-
-
-            List<Object> coordinates = new ArrayList<>();
             for (Element result : rootNode.getChildren("Result")) {
-                System.out.println(" ");
-                double i = 9;
+                int yWert = 9;
                 for (Element value : result.getChildren("Value")) {
                     String scale = value.getAttributeValue("Scale");
+                    scalesName[yWert] = scale;
                     Double v = Double.parseDouble(value.getAttributeValue("V"));
-                    coordinates.add(scale);
-                    coordinates.add(v);
-                    System.out.println("Scale: "+scale+" V: "+v);
-
-
-                    xySeries.add(i, v, 0, 0);
-                    i--;
+                    resultYSeries.add(yWert, v, 0, 0);
+                    yWert--;
                 }
             }
-
-
 
             for (Element report : rootNode.getChildren("Report")) {
-                System.out.println(" ");
-                double i = 9;
+                int yWert = 9;
                 for (Element value : report.getChildren("Value")) {
-
-                    String scale = value.getAttributeValue("Scale");
                     Double av = Double.parseDouble(value.getAttributeValue("AV"));
                     Double dev = Double.parseDouble(value.getAttributeValue("DEV"));
-                    coordinates.add(scale);
-                    coordinates.add(av);
-                    coordinates.add(dev);
-                    System.out.println("Scale: "+scale+" AV: "+av+" DEV: "+dev);
 
                     if (report.getAttributeValue("title").equals("Referenzwerte stationärer Psychotherapiepatienten")) {
-                        xySeries1.add(i, av, av-dev, av+dev);
+                        stationaerYSeries.add(yWert, av, av-dev, av+dev);
                     } else if (report.getAttributeValue("title").equals("Referenzwerte von Hausarztpatienten (Gesamtstichprobe)")) {
-                        xySeries2.add(i, av, av-dev, av+dev);
+                        hausGesamtYSeries.add(yWert, av, av-dev, av+dev);
                     } else if (report.getAttributeValue("title").equals("Referenzwerte von Hausarztpatienten (Teilstichprobe Gesunde)")) {
-                        xySeries3.add(i, av, av-dev, av+dev);
+                        hausTeilYSeries.add(yWert, av, av-dev, av+dev);
                     }
-                    i--;
+                    yWert--;
                 }
             }
-
-            dataset.addSeries(xySeries);
-            dataset.addSeries(xySeries1);
-            dataset.addSeries(xySeries2);
-            dataset.addSeries(xySeries3);
 
             JFreeChart chart = ChartFactory.createXYLineChart(
                     "Patient Values",
-                    "Scale",
                     "Value",
+                    "Scale",
                     dataset,
                     PlotOrientation.HORIZONTAL,
                     false,
@@ -103,31 +79,74 @@ public class XMLParser {
                     false
             );
 
-            XYPlot plot = (XYPlot) chart.getPlot();
-
+            XYPlot plot = chart.getXYPlot();
 
             DeviationRenderer renderer = new DeviationRenderer(true, false);
-            renderer.setSeriesStroke(0, new BasicStroke(3.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
-            renderer.setSeriesStroke(1, new BasicStroke(3.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
-            renderer.setSeriesStroke(2, new BasicStroke(3.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
-            renderer.setSeriesStroke(3, new BasicStroke(3.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
-            renderer.setSeriesFillPaint(1, new Color(34, 139, 34));
-            renderer.setSeriesFillPaint(2, new Color(0, 71, 171));
-            renderer.setSeriesFillPaint(3, new Color(136, 8, 8));
             plot.setRenderer(renderer);
 
-            SymbolAxis rangeAxis = new SymbolAxis("Value", scaleValue);
+            JComboBox comboBox = new JComboBox();
+            for (Element report : rootNode.getChildren("Report")) {
+                comboBox.addItem(report.getAttributeValue("title"));
+            }
+            comboBox.addItem("Alle Graphen");
+
+            comboBox.addActionListener(e -> {
+                switch (comboBox.getSelectedIndex()) {
+                    case 0:
+                        dataset.removeAllSeries();
+                        dataset.addSeries(resultYSeries);
+                        dataset.addSeries(stationaerYSeries);
+                        renderer.setSeriesStroke(0, new BasicStroke(3.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+                        renderer.setSeriesStroke(1, new BasicStroke(3.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+                        renderer.setSeriesFillPaint(1, new Color(136, 8, 8));
+                        break;
+                    case 1:
+                        dataset.removeAllSeries();
+                        dataset.addSeries(resultYSeries);
+                        dataset.addSeries(hausGesamtYSeries);
+                        renderer.setSeriesStroke(0, new BasicStroke(3.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+                        renderer.setSeriesStroke(1, new BasicStroke(3.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+                        break;
+                    case 2:
+                        dataset.removeAllSeries();
+                        dataset.addSeries(resultYSeries);
+                        dataset.addSeries(hausTeilYSeries);
+                        renderer.setSeriesStroke(0, new BasicStroke(3.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+                        renderer.setSeriesStroke(1, new BasicStroke(3.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+                        renderer.setSeriesFillPaint(1, new Color(136, 8, 8));
+                        break;
+                    default:
+                        dataset.removeAllSeries();
+                        dataset.addSeries(resultYSeries);
+                        dataset.addSeries(stationaerYSeries);
+                        dataset.addSeries(hausGesamtYSeries);
+                        dataset.addSeries(hausTeilYSeries);
+                        renderer.setSeriesStroke(0, new BasicStroke(3.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+                        renderer.setSeriesStroke(1, new BasicStroke(3.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+                        renderer.setSeriesStroke(2, new BasicStroke(3.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+                        renderer.setSeriesStroke(3, new BasicStroke(3.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+                        renderer.setSeriesFillPaint(1, new Color(34, 139, 34 ));
+                        renderer.setSeriesFillPaint(2, new Color(0, 71, 171 ));
+                        renderer.setSeriesFillPaint(3, new Color(136, 8, 8));
+                        break;
+                }
+            });
+
+            SymbolAxis rangeAxis = new SymbolAxis("Scale", scalesName);
             rangeAxis.setTickUnit(new NumberTickUnit(1));
             rangeAxis.setRange(0,9);
             plot.setRangeAxis(rangeAxis);
 
             ChartFrame frame = new ChartFrame("Patient Values", chart);
+            JPanel panel = new JPanel();
+
+            panel.add(comboBox);
+            frame.add(panel);
             frame.pack();
             frame.setVisible(true);
 
-
-        } catch (IOException | JDOMException e) {
-            e.printStackTrace();
+        } catch (IOException | JDOMException exception) {
+            exception.printStackTrace();
         }
     }
 }
